eabf6f9d687a38625cd4d6c65cfa18ec93b9babe
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-12-14 05:50:18 +0900

    Update Almost-Sorted.cs

--- Almost-Sorted.cs
+++ Almost-Sorted.cs
@@ -22,6 +22,40 @@ class Result
     }    
 
     public static void almostSorted(List<int> arr)
+    {
+        int n = arr.Count;
+        List<int> sortedArr = new List<int>(arr);
+        sortedArr.Sort();
+
+        if (arr.SequenceEqual(sortedArr))
+        {
+            Console.WriteLine("yes");
+            return;
+        }
+
+        int l = 0, r = n - 1;
+        while (l < n && arr[l] == sortedArr[l]) l++;
+        while (r > l && arr[r] == sortedArr[r]) r--;
+
+        Swap(arr, l, r);
+        if (arr.SequenceEqual(sortedArr))
+        {
+            Console.WriteLine("yes");
+            Console.WriteLine($"swap {l + 1} {r + 1}");
+            return;
+        }
+
+        Swap(arr, l, r);
+        arr.Reverse(l, r - l + 1);
+        if (arr.SequenceEqual(sortedArr))
+        {
+            Console.WriteLine("yes");
+            Console.WriteLine($"reverse {l + 1} {r + 1}");
+            return;
+        }
+
+        Console.WriteLine("no");
+    }
 }
 
 
