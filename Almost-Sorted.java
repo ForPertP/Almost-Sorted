0e16d5bed46592a5271881066ea666b00b392d52
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-12-15 07:34:14 +0900

    Update Almost-Sorted.java

--- Almost-Sorted.java
+++ Almost-Sorted.java
@@ -12,3 +12,21 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 }
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.almostSorted(arr);
+
+        bufferedReader.close();
+    }
+}
